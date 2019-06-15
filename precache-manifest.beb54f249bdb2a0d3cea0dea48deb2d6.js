ef17f01ab90e785a9f2c5707c020079be46f6477
arbazz <51861466+arbazz@users.noreply.github.com>
2019-06-15 23:03:38 +0500

    My first commit! 😀

---
+++ precache-manifest.beb54f249bdb2a0d3cea0dea48deb2d6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70baca7befdc00a8dd4f5ffbabd0437d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "201c328a206d4aa7155e",
+    "url": "/static/css/main.d2abac13.chunk.css"
+  },
+  {
+    "revision": "18fcdf3c0100fd46d7dd",
+    "url": "/static/js/2.d68256cb.chunk.js"
+  },
+  {
+    "revision": "201c328a206d4aa7155e",
+    "url": "/static/js/main.f197789f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "098a1225537ddb3c175dbb93550fdf33",
+    "url": "/static/media/biryani.098a1225.gif"
+  },
+  {
+    "revision": "18827609cd15c4f30bc11cf4d9999b7c",
+    "url": "/static/media/cover.18827609.jpg"
+  }
+]);
